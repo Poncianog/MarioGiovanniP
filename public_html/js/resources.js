5d6f047091b7dffc439e030a83a2a81aab532951
Giovanni Ponciano <ponciano.giovanni15@gmail.com>
2014-11-19 11:30:18 -0800

    pushing6

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -18,7 +18,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        {name: "Giolevel01", type: "tmx", src: "data/map/Giolevel01.tmx"}
+        {name: "Giolevel01", type: "tmx", src: "data/map/Giolevel01.tmx"},
+        {name: "Giolevel02", type: "tmx", src: "data/map/Giolevel02.tmx"},
 
 	/* Background music. 
 	 * @example
